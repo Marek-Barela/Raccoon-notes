9ac0405fbcdd724ec846f4e16b675fad50d42e59
Marek Bareła <marek.barela@wp.pl>
2019-07-07 12:01:52 +0200

    authentication functionality commited

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../../middleware/auth");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const { check, validationResult } = require('express-validator');
 
 const User = require("../../models/User");
 
@@ -15,6 +19,61 @@ router.get("/", auth, async (req, res) => {
   catch (err) {
     console.error(err.message);
     res.status(500).send("Server error")
+  }
+})
+
+// @route     POST api/auth
+// @desc      Authenticate user
+// @access    Public
+router.post("/", [
+  check("email", "Please include a valid email")
+    .isEmail(),
+  check("password", "Password is required")
+    .exists()
+], async (req, res) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() })
+  }
+
+  const { email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+
+    if(!user) {
+      return res
+        .status(400)
+        .json({ errors: [{msg: "Invalid Credentials"}]})
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if(!isMatch) {
+      return res
+        .status(400)
+        .json({ errors: [{msg: "Invalid Credentials"}] })
+    }
+
+    const payload = {
+      user: {
+        id: user.id
+      }
+    }
+
+    jwt.sign(
+      payload, 
+      config.get("jwtSecret"),
+      { expiresIn: 36000 },
+      (err, token) => {
+        if(err) throw err;
+        res.json({ token });
+      }
+    );
+  }
+  catch(err) {
+    console.log(err.message);
+    res.status(500).send("Server error");
   }
 })
 
