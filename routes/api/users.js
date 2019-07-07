ce1241ae85d0f89cf5b128acaf916311213f2321
Marek Bareła <marek.barela@wp.pl>
2019-07-07 08:23:34 +0200

    user registration functionality commited

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
 const { check, validationResult } = require('express-validator');
+
+const User = require("../../models/User");
 
 // @route     POST api/users
 // @desc      Register User
@@ -13,12 +16,41 @@ router.post("/", [
     .isEmail(),
   check("password", "Please enter a password with 6 or more characters")
     .isLength({ min: 6})
-], (req, res) => {
+], async (req, res) => {
   const errors = validationResult(req);
   if(!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() })
   }
-  res.send("test")
+
+  const { name, email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+
+    if(user) {
+      return res
+        .status(400)
+        .json({ errors: [{msg: "User already exists"}]})
+    }
+
+    user = new User({
+      name,
+      email,
+      password
+    })
+
+    const salt = await bcrypt.genSalt(10);
+
+    user.password = await bcrypt.hash(password, salt);
+
+    await user.save();
+
+    res.send("User registered")
+  }
+  catch(err) {
+    console.log(err.message);
+    res.status(500).send("Server error");
+  }
 })
 
 module.exports = router;
