05a070c1e003419d20c158f7b7b3b0a7f7bc3bf1
Marek Bareła <marek.barela@wp.pl>
2019-07-07 08:46:20 +0200

    jwt configuration commited

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const { check, validationResult } = require('express-validator');
 
 const User = require("../../models/User");
@@ -45,7 +47,21 @@ router.post("/", [
 
     await user.save();
 
-    res.send("User registered")
+    const payload = {
+      user: {
+        id: user.id
+      }
+    }
+
+    jwt.sign(
+      payload, 
+      config.get("jwtSecret"),
+      { expiresIn: 36000 },
+      (err, token) => {
+        if(err) throw err;
+        res.json({ token });
+      }
+    );
   }
   catch(err) {
     console.log(err.message);
