cdba53da716f14f762a6b703039c24998c583f32
Marek Bareła <marek.barela@wp.pl>
2019-07-07 13:26:49 +0200

    notes route commited

--- routes/api/notes.js
+++ routes/api/notes.js
@@ -1,11 +1,41 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const auth = require("../../middleware/auth");
 
-// @route     GET api/notes
-// @desc      Test Route
-// @access    Public
-router.get("/", (req, res) => {
-  res.send("notes")
+const Note = require("../../models/Notes");
+
+// @route     POST api/notes
+// @desc      Create note
+// @access    Private
+router.post("/", [ 
+  auth, 
+  [
+    check("title", "Title of the note is required")
+      .not()
+      .isEmpty()
+  ]
+], async (req, res) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  try {
+    const newNote = new Note ({
+      title: req.body.title,
+      text: req.body.text,
+      user: req.user.id
+    });
+
+    const note = await newNote.save();
+
+    res.json(note)
+  } 
+  catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
 })
 
 module.exports = router;
