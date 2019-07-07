89e4ac867ffdc49e243a161fa8d37d2ea56b1136
Marek Bareła <marek.barela@wp.pl>
2019-07-07 06:38:41 +0200

    initial setup commited

---
+++ server.js
@@ -0,0 +1,12 @@
+const express  = require("express");
+const connectDB = require("./config/db");
+const app = express();
+
+app.get("/", (req, res) => res.send("API running"));
+
+//Connect Database
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
