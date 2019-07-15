842214c6ac035df3ea261832dc242002779fbc72
Marek Bareła <marek.barela@wp.pl>
2019-07-15 22:32:50 +0200

    editor buttons update

--- client/src/components/editor-buttons-container/index.js
+++ client/src/components/editor-buttons-container/index.js
@@ -2,11 +2,14 @@ import React from "react";
 import styles from "./index.module.css";
 
 const EditorButtonsContainer = () => {
-  const { wrapper, accept, deceline } = styles;
+  const { wrapper, accept, danger } = styles;
   return (
     <div className={wrapper}>
-      <button className={accept}>Save</button>
-      <button className={deceline}>Cancel</button>
+      <button className={danger}>x</button>
+      <div>
+        <button className={accept}>Save</button>
+        <button>Cancel</button>
+      </div>
     </div>
   );
 };
