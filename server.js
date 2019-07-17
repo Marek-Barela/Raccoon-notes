36fc312d58dcee659ad71e39c98ecef070d910c6
Marek Bareła <marek.barela@wp.pl>
2019-07-17 16:14:09 +0200

    script and server update

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
-const express  = require("express");
+const express = require("express");
 const connectDB = require("./config/db");
 const app = express();
+const path = require("path");
 
 //Connect Database
 connectDB();
 
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => res.send("API running"));
-
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/notes", require("./routes/api/notes"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
