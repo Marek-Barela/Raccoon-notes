033d39d2217ffc141716bf4b278d6e13af8f81b0
Marek Bareła <marek.barela@wp.pl>
2019-07-22 21:31:16 +0200

    sidebar reducer deleted from root reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,12 +3,10 @@ import alert from "./alert";
 import auth from "./auth";
 import notes from "./notes";
 import editor from "./editor";
-import sidebar from "./sidebar";
 
 export default combineReducers({
   alert,
   auth,
   notes,
-  editor,
-  sidebar
+  editor
 });
